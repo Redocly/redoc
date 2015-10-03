10e8f199d2b97daf09eb4e8b67c4dd27fe394a66
Roman Gotsiy <gotsijroman@gmail.com>
2015-10-03 15:24:12 +0300

    Set up travis build and gh-pages deploy

---
+++ deploy_gh_pages.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+set -o pipefail
+(
+  set -e
+  set -x
+  cd demo
+  git init
+  git config user.name "Travis-CI"
+  git config user.email "travis@travis"
+  cp -r ../dist ./dist
+  git add .
+  git commit -m "Deployed to Github Pages"
+  git push --force "https://${GH_TOKEN}@${GH_REF}" master:gh-pages > /dev/null 2>&1
+) 2>&1 | sed "s/${GH_TOKEN}/xxPASSxx/"
