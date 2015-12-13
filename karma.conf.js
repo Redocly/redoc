6b4c7e44e3984e6d076f1e0cd3e99f30e978e8e2
Roman Hotsiy <gotsijroman@gmail.com>
2015-12-13 12:37:58 +0200

    Setup coverage and coveralls

--- karma.conf.js
+++ karma.conf.js
@@ -1,7 +1,37 @@
 module.exports = function (config) {
+    var travis = process.env.TRAVIS;
     config.set({
         frameworks: ['phantomjs-shim', 'jspm', 'mocha', 'chai', 'sinon'],
+        preprocessors: {
+          'lib/**/!(*spec).js': ['babel', 'coverage']
+        },
+        babelPreprocessor: {
+            options: {
+                sourceMap: 'inline',
+                "optional": [
+                  "runtime",
+                  "optimisation.modules.system",
+                  "es7.decorators",
+                  "es7.classProperties"
+                ]
+            },
+            sourceFileName: function(file) {
+              return file.originalPath;
+            }
+        },
 
+        coverageReporter: {
+            instrumenters: { isparta : require('isparta') },
+            instrumenter: {
+                'lib/**/!(*spec).js': 'isparta'
+            },
+            dir: 'coverage/',
+            reporters: [
+                {type: 'html'},
+                {type: 'text-summary'},
+                {type: 'lcov'}
+            ]
+        },
         client: {
           chai: {
             truncateThreshold: 0
@@ -27,7 +57,7 @@ module.exports = function (config) {
             '/jspm_packages/': '/base/jspm_packages/',
             '/node_modules/': '/base/node_modules/',
         },
-        reporters: ['mocha'],
+        reporters: travis ? ['mocha', 'coverage', 'coveralls'] : ['mocha', 'coverage'],
 
         browsers: ['PhantomJS'],
 
