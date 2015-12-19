c591f754683e3308d9620e1298213a412bc38ad6
Roman Hotsiy <gotsijroman@gmail.com>
2015-12-19 14:16:29 +0200

    Add Zippy component spec

--- tests/helpers.js
+++ tests/helpers.js
@@ -9,3 +9,12 @@ export function getChildDebugElement(parent, tagName) {
     return debugEl.nativeElement.tagName && debugEl.nativeElement.tagName.toLowerCase() === tagName;
   });
 }
+
+export function mouseclick( element ) {
+    // create a mouse click event
+    var event = document.createEvent( 'MouseEvents' );
+    event.initMouseEvent( 'click', true, true, window, 1, 0, 0 );
+
+    // send click to element
+    element.dispatchEvent( event );
+}
