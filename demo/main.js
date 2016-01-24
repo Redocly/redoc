deed1272fce7e76e066ce84319268576d11f1699
Roman Hotsiy <gotsijroman@gmail.com>
2016-01-24 19:01:17 +0200

    add loading animation

--- demo/main.js
+++ demo/main.js
@@ -4,7 +4,8 @@
   var schemaUrlForm = document.getElementById('schema-url-form');
   var schemaUrlInput = document.getElementById('schema-url-input');
   schemaUrlForm.addEventListener('submit', function(event) {
+    event.preventDefault();
     Redoc.init(schemaUrlInput.value);
-    event.preventDefault(); 
+    return false;
   })
 })();
