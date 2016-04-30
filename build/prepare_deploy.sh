3405e07d3103f03125b6e5fb9f6c11f90178b9b5
Roman Hotsiy <gotsijroman@gmail.com>
2016-04-30 23:41:54 +0300

    Fix deployment script

--- build/prepare_deploy.sh
+++ build/prepare_deploy.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
 set -e
 set -x
+# copy old releases
 mkdir -p .ghpages-tmp && cd .ghpages-tmp
+# reset local changes before checkout
+git reset --hard
+git fetch origin gh-pages:gh-pages
 git checkout gh-pages
 cp -R ../releases/* .
 git checkout master
 cd -
+
+# build
 gulp build
 cd demo
 cp -R ../dist ./dist
