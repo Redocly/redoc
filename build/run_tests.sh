cc422559fe2d6d65d459c15037892ed52420ca76
Roman Hotsiy <gotsijroman@gmail.com>
2016-05-10 11:02:47 +0300

    include basic e2e to the tescases needed for deployment

--- build/run_tests.sh
+++ build/run_tests.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
-if [ "$JOB" = "e2e" ] || [ "$JOB" = "e2e-guru" ]; then
+if [ "$JOB" = "e2e-guru" ]; then
   npm run e2e
 else
   npm run unit
+  echo "Starting Basic E2E"
+  npm run e2e
 fi
