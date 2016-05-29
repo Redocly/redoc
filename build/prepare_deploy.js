40b03e993c2f5fb9808eb89cb2f1a695d878173b
Roman Hotsiy <gotsijroman@gmail.com>
2016-05-29 20:47:38 +0300

    Simplified deployment

--- build/prepare_deploy.js
+++ build/prepare_deploy.js
@@ -5,28 +5,20 @@ require('shelljs/global');
 var paths = require('./paths');
 var path = require('path');
 
-// copy old releases
-mkdir('-p', '.ghpages-tmp');
-cd('.ghpages-tmp');
-
-// reset local changes before checkout
-exec('git reset --hard');
-exec('git fetch origin gh-pages:gh-pages');
-exec('git checkout gh-pages');
-cp('-R', '../releases/*', '.');
-exec('git checkout @{-1}');
-cd('..');
+set('-e');
+set('-v');
 
 // build
 exec('npm run build-dist');
 cd('demo');
 mkdir('-p', 'dist');
 cp('-R', '../dist/*', './dist/');
-mkdir('-p', 'releases');
-cp('-R', '../.ghpages-tmp/*', './releases/');
 cd('..');
+
 var version = 'v' + require(path.join(__dirname, '../package.json')).version + '/';
 var versionDir = path.join(paths.releases, version);
+var latestDir = path.join(paths.releases, 'latest/');
 mkdir('-p', versionDir)
+mkdir('-p', latestDir);
 cp(paths.redocBuilt + '.min.js', versionDir);
-cp(paths.redocBuilt + '.min.js', path.join(paths.releases, 'latest/'));
+cp(paths.redocBuilt + '.min.js', latestDir);
