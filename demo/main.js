c076b5ebbc4ac35fcdefb8c626eb3aa7dea76da6
Roman Hotsiy <gotsijroman@gmail.com>
2016-07-29 13:40:02 +0300

    Enhance demo (fixes #49)

--- demo/main.js
+++ demo/main.js
@@ -2,19 +2,12 @@
   'use strict';
 
   var schemaUrlForm = document.getElementById('schema-url-form');
-  var schemaUrlInput = document.getElementById('schema-url-input');
-  schemaUrlForm.addEventListener('submit', function(event) {
-    event.preventDefault();
-    event.stopPropagation();
-    location.search = updateQueryStringParameter(location.search, 'url', schemaUrlInput.value)
-    return false;
-  })
+  var schemaUrlInput;
 
   var url = window.location.search.match(/url=([^&]+)/);
   if (url && url.length > 1) {
     url = decodeURIComponent(url[1]);
     document.getElementsByTagName('redoc')[0].setAttribute('spec-url', url);
-    schemaUrlInput.value = url;
   }
 
   function updateQueryStringParameter(uri, key, value) {
@@ -31,5 +24,35 @@
       return uri + separator + key + "=" + value + hash;
     }
   }
+
+  var specs = document.querySelector('#specs');
+  specs.addEventListener('dom-change', function() {
+    schemaUrlForm = document.getElementById('schema-url-form');
+    schemaUrlInput = document.getElementById('schema-url-input');
+
+    schemaUrlForm.addEventListener('submit', function(event) {
+      console.log('test')
+      event.preventDefault();
+      event.stopPropagation();
+      location.search = updateQueryStringParameter(location.search, 'url', schemaUrlInput.value)
+      return false;
+    })
+
+    schemaUrlInput.addEventListener('mousedown', function(e) {
+      e.stopPropagation();
+    });
+    schemaUrlInput.value = url;
+    specs.specs = [
+      'https://api.apis.guru/v2/specs/instagram.com/1.0.0/swagger.yaml',
+      'https://api.apis.guru/v2/specs/googleapis.com/calendar/v3/swagger.yaml',
+      'https://api.apis.guru/v2/specs/data2crm.com/1/swagger.yaml',
+      'https://api.apis.guru/v2/specs/graphhopper.com/1.0/swagger.yaml'
+    ];
+    var $specInput = document.getElementById('spec-input');
+    $specInput.addEventListener('value-changed', function(e) {
+      schemaUrlInput.value = e.detail.value;
+      location.search = updateQueryStringParameter(location.search, 'url', schemaUrlInput.value);
+    });
+  });
   //window.redocDebugMode = true;
 })();
