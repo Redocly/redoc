8300aa60556dd75c6f96bf3f28469bbeb73a287a
Roman Hotsiy <gotsijroman@gmail.com>
2016-08-01 07:04:35 +0300

    Fix tests failure on PR

--- build/run_tests.js
+++ build/run_tests.js
@@ -4,10 +4,22 @@
 require('shelljs/global');
 set('-e');
 
+function isPR() {
+  return process.env.TRAVIS_PULL_REQUEST && process.env.TRAVIS_PULL_REQUEST !== 'false';
+}
+
 if (process.env.JOB === 'e2e-guru') {
+  if (isPR()) {
+    console.log('Skiping E2E tests on PR');
+    return;
+  }
   exec('npm run e2e');
 } else {
   exec('npm run unit');
+  if (isPR()) {
+    console.log('Skiping E2E tests on PR');
+    return;
+  }
   console.log('Starting Basic E2E');
   exec('npm run e2e');
 }
