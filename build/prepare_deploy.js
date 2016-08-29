e6efe3559527208b136c1224cfb967c24dd35771
Roman Hotsiy <gotsijroman@gmail.com>
2016-08-29 09:36:53 +0300

    Creating CDN major release 1.x.x (#87)

--- build/prepare_deploy.js
+++ build/prepare_deploy.js
@@ -19,7 +19,10 @@ cd('..');
 var version = 'v' + require(path.join(__dirname, '../package.json')).version + '/';
 var versionDir = path.join(paths.releases, version);
 var latestDir = path.join(paths.releases, 'latest/');
+var v1Dir = path.join(paths.releases, '1.x.x/');
 mkdir('-p', versionDir)
 mkdir('-p', latestDir);
+mkdir('-p', v1Dir);
 cp(paths.redocBuilt + '.min.js', versionDir);
 cp(paths.redocBuilt + '.min.js', latestDir);
+cp(paths.redocBuilt + '.min.js', v1Dir);
