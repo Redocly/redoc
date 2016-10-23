5583c8a3133db3a5fe3a800b7710cd712dd1ae86
Roman Hotsiy <gotsijroman@gmail.com>
2016-10-23 20:36:34 +0300

    Enabled a few codelyzer rules

--- lib/services/schema-helper.service.ts
+++ lib/services/schema-helper.service.ts
@@ -1,7 +1,7 @@
 'use strict';
 import { JsonPointer } from '../utils/JsonPointer';
 import { SpecManager } from '../utils/spec-manager';
-import {methods as swaggerMethods, keywordTypes} from  '../utils/swagger-defs';
+import { methods as swaggerMethods, keywordTypes } from  '../utils/swagger-defs';
 import { WarningsService } from './warnings.service';
 import * as slugify from 'slugify';
 
