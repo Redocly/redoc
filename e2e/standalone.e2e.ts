1dabeebb3d0f47ffb4ea42441e8426d7a716348e
Roman Hotsiy <gotsijroman@gmail.com>
2017-11-20 13:55:32 +0200

    Do not reload page extra times in e2e tests

--- e2e/standalone.e2e.ts
+++ e2e/standalone.e2e.ts
@@ -1,33 +1,29 @@
 describe('Standalone bundle test', function() {
   describe('OAS3 mode', () => {
+    before(() => {
+      cy.visit('e2e/standalone.html');
+    });
+
     it('Render and check no errors', function() {
-      cy
-        .visit('e2e/standalone.html')
-        .get('.api-info')
-        .should('exist');
+      cy.get('.api-info').should('exist');
     });
 
     it('Render and click all the menu items', function() {
-      cy
-        .visit('e2e/standalone.html')
-        .get('.menu-content li')
-        .click({ multiple: true, force: true });
+      cy.get('.menu-content li').click({ multiple: true, force: true });
     });
   });
 
   describe('OAS2 compatibility mode', () => {
+    before(() => {
+      cy.visit('e2e/standalone.html');
+    });
+
     it('Render and check no errors', function() {
-      cy
-        .visit('e2e/standalone.html')
-        .get('.api-info')
-        .should('exist');
+      cy.get('.api-info').should('exist');
     });
 
     it('Render and click all the menu items', function() {
-      cy
-        .visit('e2e/standalone.html')
-        .get('.menu-content li')
-        .click({ multiple: true, force: true });
+      cy.get('.menu-content li').click({ multiple: true, force: true });
     });
   });
 });
