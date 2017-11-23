86a7405ebf6300f9f0538abfedf1bb249e150796
Roman Hotsiy <gotsijroman@gmail.com>
2017-11-23 13:40:21 +0200

    Make conversion process more robust

--- src/utils/loadAndBundleSpec.ts
+++ src/utils/loadAndBundleSpec.ts
@@ -18,7 +18,7 @@ export async function loadAndBundleSpec(specUrlOrObject: object | string): Promi
 export function convertSwagger2OpenAPI(spec: any): Promise<OpenAPISpec> {
   console.warn('[ReDoc Compatibility mode]: Converting OpenAPI 2.0 to OpenAPI 3.0');
   return new Promise<OpenAPISpec>((resolve, reject) =>
-    convertObj(spec, {}, (err, res) => {
+    convertObj(spec, { patch: true, warnOnly: true }, (err, res) => {
       // TODO: log any warnings
       if (err) {
         return reject(err);
