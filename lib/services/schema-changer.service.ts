59939902a34d32bc36d8fc20902efba20a3ff137
Craig Larrabee <craiglarrabee@ldschurch.org>
2017-12-08 11:38:53 -0500

    Added Observable import to schema-changer.service.ts

--- lib/services/schema-changer.service.ts
+++ lib/services/schema-changer.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Subject }    from 'rxjs/Subject';
+import { Observable } from '@angular/core';
 
 @Injectable()
 export class SchemaChangerService {
