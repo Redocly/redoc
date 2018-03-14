c22abaafb50c975f9e8c475b5059b2b604283cbf
Zach Pomerantz <zmp@umich.edu>
2018-03-15 12:09:58 +1300

    add favicon

--- docker-run.sh
+++ docker-run.sh
@@ -6,6 +6,12 @@ if [ -n "$API_TITLE" ]; then
   sed -i "s|%API_TITLE%|$API_TITLE|g" /usr/share/nginx/html/index.html
 fi
 
+if [ -n "$API_FAVICON" ]; then
+  sed -i "s|%API_FAVICON%|<link rel=\"icon\" href=\"$API_FAVICON\">|g" /usr/share/nginx/html/index.html
+else
+  sed -i "|%API_FAVICON%|d" /usr/share/nginx/html/index.html
+fi
+
 if [ -n "$API_URL" ]; then
   sed -i "s|%API_URL%|$API_URL|g" /usr/share/nginx/html/index.html
 fi
