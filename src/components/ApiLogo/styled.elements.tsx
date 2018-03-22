384c883bb817bb55dbfaedb29ff1ca00b7bcdb8b
Roman Hotsiy <gotsijroman@gmail.com>
2018-03-22 18:50:31 +0200

    fix: fix logo width

--- src/components/ApiLogo/styled.elements.tsx
+++ src/components/ApiLogo/styled.elements.tsx
@@ -4,6 +4,7 @@ import styled from '../../styled-components';
 export const LogoImgEl = styled.img`
   max-height: ${props => props.theme.logo.maxHeight};
   max-width: ${props => props.theme.logo.maxWidth};
+  width: 100%;
   display: block;
 `;
 
