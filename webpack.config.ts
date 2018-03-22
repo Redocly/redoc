3fabce4d1912671c97bed17cfa4e4f5b37b1b2b9
Roman Hotsiy <gotsijroman@gmail.com>
2018-03-22 18:51:16 +0200

    chore: disable source-maps for css loader

--- webpack.config.ts
+++ webpack.config.ts
@@ -16,12 +16,10 @@ const REVISION = JSON.stringify(
     .trim(),
 );
 
-const BANNER =
-  `ReDoc - OpenAPI/Swagger-generated API Reference Documentation
+const BANNER = `ReDoc - OpenAPI/Swagger-generated API Reference Documentation
 -------------------------------------------------------------
   Version: ${VERSION}
   Repo: https://github.com/Rebilly/ReDoc`;
-
 
 export default (env: { standalone?: boolean } = {}) => ({
   entry: env.standalone ? ['./src/polyfills.ts', './src/standalone.tsx'] : './src/index.ts',
@@ -51,14 +49,14 @@ export default (env: { standalone?: boolean } = {}) => ({
 
   externals: env.standalone
     ? {
-      esprima: 'esprima',
-      'node-fetch': 'null',
-    }
+        esprima: 'esprima',
+        'node-fetch': 'null',
+      }
     : (context, request, callback) => {
-      // ignore node-fetch dep of swagger2openapi as it is not used
-      if (/node-fetch$/i.test(request)) return callback(null, 'var undefined');
-      return nodeExternals(context, request, callback);
-    },
+        // ignore node-fetch dep of swagger2openapi as it is not used
+        if (/node-fetch$/i.test(request)) return callback(null, 'var undefined');
+        return nodeExternals(context, request, callback);
+      },
 
   module: {
     rules: [
@@ -92,7 +90,7 @@ export default (env: { standalone?: boolean } = {}) => ({
         use: {
           loader: 'css-loader',
           options: {
-            sourceMap: true,
+            sourceMap: false,
             minimize: true,
           },
         },
@@ -106,6 +104,6 @@ export default (env: { standalone?: boolean } = {}) => ({
       __REDOC_REVISION__: REVISION,
     }),
     new ForkTsCheckerWebpackPlugin({ silent: true }),
-    new webpack.BannerPlugin(BANNER)
+    new webpack.BannerPlugin(BANNER),
   ],
 });
