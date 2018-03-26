5cab571f78f5e6b1b6293433cc7e6cf6adcf2a68
Roman Hotsiy <gotsijroman@gmail.com>
2018-03-26 13:58:20 +0300

    chore: fix lint issues

--- src/components/Responses/ResponsesList.tsx
+++ src/components/Responses/ResponsesList.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
-import styled from '../../styled-components';
 import { ResponseModel } from '../../services/models';
+import styled from '../../styled-components';
 import { ResponseView } from './Response';
 
 const ResponsesHeader = styled.h3`
