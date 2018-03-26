d5286fc5bbc04c90798d5d704a7fb339bb76c834
Roman Hotsiy <gotsijroman@gmail.com>
2018-03-26 13:57:39 +0300

    chore: exclude some rarely-used langs for APIs (-4Kb)

--- src/utils/highlight.ts
+++ src/utils/highlight.ts
@@ -1,21 +1,17 @@
 import * as Prism from 'prismjs';
-import 'prismjs/components/prism-actionscript.js';
 import 'prismjs/components/prism-bash.js';
 import 'prismjs/components/prism-c.js';
 import 'prismjs/components/prism-coffeescript.js';
 import 'prismjs/components/prism-cpp.js';
 import 'prismjs/components/prism-csharp.js';
 import 'prismjs/components/prism-go.js';
-import 'prismjs/components/prism-haskell.js';
 import 'prismjs/components/prism-java.js';
 import 'prismjs/components/prism-lua.js';
 import 'prismjs/components/prism-markup.js'; // xml
-import 'prismjs/components/prism-matlab.js';
 import 'prismjs/components/prism-objectivec.js';
 import 'prismjs/components/prism-perl.js';
 import 'prismjs/components/prism-php.js';
 import 'prismjs/components/prism-python.js';
-import 'prismjs/components/prism-r.js';
 import 'prismjs/components/prism-ruby.js';
 import 'prismjs/components/prism-scala.js';
 import 'prismjs/components/prism-swift.js';
