3829aca046e23711a1755a74ebfe6eb4b58d695d
Roman Hotsiy <gotsijroman@gmail.com>
2018-05-14 11:00:43 +0300

    chore: fix lint issues

--- src/utils/highlight.ts
+++ src/utils/highlight.ts
@@ -1,17 +1,17 @@
 import * as Prism from 'prismjs';
-import 'prismjs/components/prism-clike.js';
 import 'prismjs/components/prism-bash.js';
 import 'prismjs/components/prism-c.js';
+import 'prismjs/components/prism-clike.js';
 import 'prismjs/components/prism-coffeescript.js';
 import 'prismjs/components/prism-cpp.js';
 import 'prismjs/components/prism-csharp.js';
 import 'prismjs/components/prism-go.js';
 import 'prismjs/components/prism-java.js';
 import 'prismjs/components/prism-lua.js';
+import 'prismjs/components/prism-markup-templating.js'; // dep of php
 import 'prismjs/components/prism-markup.js'; // xml
 import 'prismjs/components/prism-objectivec.js';
 import 'prismjs/components/prism-perl.js';
-import 'prismjs/components/prism-markup-templating.js'; // dep of php
 import 'prismjs/components/prism-php.js';
 import 'prismjs/components/prism-python.js';
 import 'prismjs/components/prism-ruby.js';
