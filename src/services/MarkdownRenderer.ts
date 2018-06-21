65cf9edb88486040b378d8a88f8c7b5f344e7428
Roman Hotsiy <gotsijroman@gmail.com>
2018-06-21 12:04:23 +0300

    undo not related changes

--- src/services/MarkdownRenderer.ts
+++ src/services/MarkdownRenderer.ts
@@ -35,7 +35,7 @@ export class MarkdownRenderer {
 
   constructor() {
     this.headingEnhanceRenderer = new marked.Renderer();
-    this.originalHeadingRule = this.headingEnhanceRenderer.heading.bind(this.headingEnhanceRenderer);
+    this.originalHeadingRule = this.headingEnhanceRenderer.heading;
     this.headingEnhanceRenderer.heading = this.headingRule;
   }
 
