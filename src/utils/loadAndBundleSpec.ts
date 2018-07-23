b5d0aca9aec5bd2d0c3660d52f0bc9191326a04f
Roman Hotsiy <gotsijroman@gmail.com>
2018-07-23 13:22:24 +0300

    chore: fix lint issues

--- src/utils/loadAndBundleSpec.ts
+++ src/utils/loadAndBundleSpec.ts
@@ -1,4 +1,5 @@
 import * as JsonSchemaRefParser from 'json-schema-ref-parser';
+/* tslint:disable-next-line:no-implicit-dependencies */
 import { convertObj } from 'swagger2openapi';
 import { OpenAPISpec } from '../types';
 
