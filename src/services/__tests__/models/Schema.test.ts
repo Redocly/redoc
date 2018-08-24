81c0df092aea266f002b4934687179d173a4cb05
Roman Hotsiy <gotsijroman@gmail.com>
2018-08-24 11:54:26 +0300

    chore: add tests for one of titles

--- src/services/__tests__/models/Schema.test.ts
+++ src/services/__tests__/models/Schema.test.ts
@@ -15,5 +15,29 @@ describe('Models', () => {
       expect(schema.oneOf).toHaveLength(1);
       expect(schema.discriminatorProp).toEqual('type');
     });
+
+    test('oneOf/allOf titles', () => {
+      const spec = require('../fixtures/oneOfTitles.json');
+      parser = new OpenAPIParser(spec, undefined, opts);
+      const schema = new SchemaModel(parser, spec.components.schemas.Test, '', opts);
+      expect(schema.fields).toHaveLength(3);
+      const oneOfField = schema.fields[0];
+      expect(oneOfField.schema.displayType).toBe('Foo (object) or Bar (object)');
+      expect(oneOfField.schema.oneOf[0].title).toBe('Foo');
+      expect(oneOfField.schema.oneOf[1].title).toBe('Bar');
+
+      const anyOfField = schema.fields[1];
+      expect(anyOfField.schema.displayType).toBe('Foo (object) or Bar (object)');
+      expect(anyOfField.schema.oneOf[0].title).toBe('Foo');
+      expect(anyOfField.schema.oneOf[1].title).toBe('Bar');
+    });
+
+    test('oneOf/allOf schema complex displayType', () => {
+      const spec = require('../fixtures/oneOfTitles.json');
+      parser = new OpenAPIParser(spec, undefined, opts);
+      const schema = new SchemaModel(parser, spec.components.schemas.WithArray, '', opts);
+      expect(schema.oneOf).toHaveLength(2);
+      expect(schema.displayType).toBe('(Array of string or number) or string');
+    });
   });
 });
