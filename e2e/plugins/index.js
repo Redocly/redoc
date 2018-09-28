d0c38cd158b592923af3b518f8d23580448d0117
Roman Hotsiy <gotsijroman@gmail.com>
2018-09-28 16:49:34 +0300

    chore: inconsistent local and travis, Revert "tests: add basic screenshot test for schema view"
    This reverts commit 10ab7dabc0898cbd6cbb13d6efcb4f1430a81db0.

--- e2e/plugins/index.js
+++ e2e/plugins/index.js
@@ -1,7 +1,5 @@
 const cypressTypeScriptPreprocessor = require('./cy-ts-preprocessor');
-const { addMatchImageSnapshotPlugin } = require('cypress-image-snapshot/plugin');
 
 module.exports = on => {
   on('file:preprocessor', cypressTypeScriptPreprocessor);
-  addMatchImageSnapshotPlugin(on);
 };
--- e2e/plugins/support.js
+++
@@ -1,6 +0,0 @@
-import { addMatchImageSnapshotCommand } from 'cypress-image-snapshot/command';
-
-addMatchImageSnapshotCommand({
-  customSnapshotsDir: 'e2e/snapshots',
-  // disableTimersAndAnimations: false,
-});
