1bbb0fe6792a9d10062b887f9a21ad3fd6b6545d
DeBr0glie <afrimuchkov@yandex.ru>
2019-01-10 11:06:51 +0300

    docker: Incorrect port after restart when PORT is 8080 (#776)
    When PORT is 8080 and dokcer restart, nginx will listen 80808080

--- config/docker/docker-run.sh
+++ config/docker/docker-run.sh
@@ -6,6 +6,6 @@ sed -i -e "s|%PAGE_TITLE%|$PAGE_TITLE|g" /usr/share/nginx/html/index.html
 sed -i -e "s|%PAGE_FAVICON%|$PAGE_FAVICON|g" /usr/share/nginx/html/index.html
 sed -i -e "s|%SPEC_URL%|$SPEC_URL|g" /usr/share/nginx/html/index.html
 sed -i -e "s|%REDOC_OPTIONS%|${REDOC_OPTIONS}|g" /usr/share/nginx/html/index.html
-sed -i -e "s|80|${PORT}|g" /etc/nginx/nginx.conf
+sed -i -e "s|\(listen\s*\) [0-9]*|\1 ${PORT}|g" /etc/nginx/nginx.conf
 
 exec nginx -g 'daemon off;'
