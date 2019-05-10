6de3523eb0976ed56797a3f0ed51bf7fc5b9173c
Roman Hotsiy <gotsijroman@gmail.com>
2019-05-10 18:03:28 +0300

    chore: fix bundle after oas-kit update

--- webpack.config.ts
+++ webpack.config.ts
@@ -65,13 +65,13 @@ export default (env: { standalone?: boolean } = {}, { mode }) => ({
     ? {
         esprima: 'esprima',
         'node-fetch': 'null',
+        'node-fetch-h2': 'null',
+        yaml: 'null',
+        'safe-json-stringify': 'null',
       }
     : (context, request, callback) => {
         // ignore node-fetch dep of swagger2openapi as it is not used
-        if (/node-fetch$/i.test(request)) {
-          return callback(null, 'var undefined');
-        }
-        if (/esprima$/i.test(request)) {
+        if (/esprima|node-fetch|node-fetch-h2|yaml|safe-json-stringify$/i.test(request)) {
           return callback(null, 'var undefined');
         }
         return nodeExternals(context, request, callback);
