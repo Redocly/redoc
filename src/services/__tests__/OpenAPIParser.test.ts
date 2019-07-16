b9c93c7061ddf17cbd6e1dae0c71ee6d80149f5c
craigmc08 <craigmc08@gmail.com>
2019-07-16 16:09:54 -0400

    Add test for new allOf circular detection case

--- src/services/__tests__/OpenAPIParser.test.ts
+++ src/services/__tests__/OpenAPIParser.test.ts
@@ -12,5 +12,13 @@ describe('Models', () => {
       parser = new OpenAPIParser(spec, undefined, opts);
       expect(parser.mergeAllOf(spec.components.schemas.test)).toMatchSnapshot();
     });
+
+    test('should not mark as circular when multiple schemas in allOf use the same ref', () => {
+      const spec = require('./fixtures/allOfCircular.json');
+      parser = new OpenAPIParser(spec, undefined, opts);
+      expect(
+        parser.mergeAllOf(spec.components.schemas.test).properties.object['x-circular-ref'],
+      ).toEqual(undefined);
+    });
   });
 });
