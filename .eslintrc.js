4ab2d0556da2c47404bc638c7195385eb695febc
Roman Hotsiy <gotsijroman@gmail.com>
2020-03-17 11:52:33 +0200

    chore: migrate tslint -> eslint

---
+++ .eslintrc.js
@@ -0,0 +1,42 @@
+module.exports = {
+  env: {
+    browser: true,
+  },
+  parser: '@typescript-eslint/parser',
+  extends: ['plugin:react/recommended', 'plugin:@typescript-eslint/recommended'],
+  parserOptions: {
+    project: 'tsconfig.json',
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+  plugins: ['@typescript-eslint', 'import'],
+  rules: {
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/no-use-before-define': 'off',
+    '@typescript-eslint/interface-name-prefix': 'off',
+    '@typescript-eslint/no-inferrable-types': 'off',
+    'react/prop-types': 'off',
+
+    'import/no-extraneous-dependencies': 'error',
+    'import/no-internal-modules': [
+      'error',
+      {
+        allow: [
+          'prismjs/**',
+          'perfect-scrollbar/**',
+          'react-dom/*',
+          'core-js/**',
+          'memoize-one/**',
+        ],
+      },
+    ],
+  },
+};
