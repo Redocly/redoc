85ecd0d4e250959a7fc4445cc7539467e498f227
akumarsingh <akumarsingh@opentext.com>
2020-03-18 15:10:03 -0400

    resolving merge conflict error

--- src/types/index.d.ts
+++ src/types/index.d.ts
@@ -2,7 +2,7 @@ export * from './open-api';
 
 export type Omit<T, K extends keyof T> = Pick<T, Exclude<keyof T, K>>;
 declare global {
-  type Dict<T> = {
-    [key: string]: T;
-  };
+  // type Dict<T> = {
+  //   [key: string]: T;
+  // };
 }
