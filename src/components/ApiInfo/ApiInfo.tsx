b59faada8210a4c8f61fa0e850b7d844574a46d1
Roman Hotsiy <gotsijroman@gmail.com>
2020-05-09 14:54:08 +0300

    fix: download button opens in new tab instead of downloading
    fixes #1247

--- src/components/ApiInfo/ApiInfo.tsx
+++ src/components/ApiInfo/ApiInfo.tsx
@@ -81,7 +81,7 @@ export class ApiInfo extends React.Component<ApiInfoProps> {
               <p>
                 Download OpenAPI specification:
                 <DownloadButton
-                  download={downloadFilename}
+                  download={downloadFilename || true}
                   target="_blank"
                   href={downloadLink}
                   onClick={this.handleDownloadClick}
