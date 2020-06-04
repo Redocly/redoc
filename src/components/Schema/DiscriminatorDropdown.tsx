70cb00a76b77bb48e183fbe055ed174cec495449
Anya Stasiuk <stasiukanya@gmail.com>
2020-06-04 16:10:30 +0300

    fix: make dropdowns accessible by keyboard

--- src/components/Schema/DiscriminatorDropdown.tsx
+++ src/components/Schema/DiscriminatorDropdown.tsx
@@ -21,7 +21,7 @@ export class DiscriminatorDropdown extends React.Component<{
     });
 
     options.sort((a, b) => {
-      return enumOrder[a.label] > enumOrder[b.label] ? 1 : -1;
+      return enumOrder[a.value] > enumOrder[b.value] ? 1 : -1;
     });
   }
 
@@ -33,21 +33,23 @@ export class DiscriminatorDropdown extends React.Component<{
 
     const options = parent.oneOf.map((subSchema, idx) => {
       return {
-        value: idx.toString(),
-        label: subSchema.title,
+        value: subSchema.title,
+        idx,
       };
     });
 
-    const activeItem = options[parent.activeOneOf];
     this.sortOptions(options, enumValues);
 
     return (
-      <StyledDropdown value={activeItem} options={options} onChange={this.changeActiveChild} />
+      <StyledDropdown
+        value={options[parent.activeOneOf].value}
+        options={options}
+        onChange={this.changeActiveChild}
+      />
     );
   }
 
-  changeActiveChild = ({ value }) => {
-    const idx = parseInt(value, 10);
-    this.props.parent.activateOneOf(idx);
+  changeActiveChild = (option: DropdownOption) => {
+    this.props.parent.activateOneOf(option.idx);
   };
 }
