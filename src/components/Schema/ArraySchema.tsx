e6a10ff0c26d6964e0df16fec0b9061827677420
Moshe Eshel <moshe.eshel@kenshoo.com>
2020-06-25 18:34:07 -0400

    add Array size info based on min max Items properties

--- src/components/Schema/ArraySchema.tsx
+++ src/components/Schema/ArraySchema.tsx
@@ -4,6 +4,7 @@ import { Schema, SchemaProps } from './Schema';
 
 import { ArrayClosingLabel, ArrayOpenningLabel } from '../../common-elements';
 import styled from '../../styled-components';
+import {humanizeConstraints} from "../../utils";
 
 const PaddedSchema = styled.div`
   padding-left: ${({ theme }) => theme.spacing.unit * 2}px;
@@ -12,9 +13,16 @@ const PaddedSchema = styled.div`
 export class ArraySchema extends React.PureComponent<SchemaProps> {
   render() {
     const itemsSchema = this.props.schema.items!;
+    const itemConstraintSchema = (
+      min: number | undefined = undefined,
+      max: number | undefined = undefined,
+    ) => ({ type: 'array', minItems: min, maxItems: max });
+
+    const minMaxItems = humanizeConstraints(itemConstraintSchema(itemsSchema.schema.minItems, itemsSchema.schema.maxItems));
+
     return (
       <div>
-        <ArrayOpenningLabel> Array </ArrayOpenningLabel>
+        <ArrayOpenningLabel> Array ({minMaxItems})</ArrayOpenningLabel>
         <PaddedSchema>
           <Schema {...this.props} schema={itemsSchema} />
         </PaddedSchema>
