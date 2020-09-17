5d35a7ec60f59290a4a8821d7764a21440bbdf73
davew <davew@opentext.com>
2020-09-17 11:27:41 -0700

    problem with code tag on json and use COPYFILES instead of Mac CP command

--- src/utils/jsonToHtml.ts
+++ src/utils/jsonToHtml.ts
@@ -4,9 +4,9 @@ export function jsonToHTML(json, maxExpandLevel) {
   level = 1;
   let output = '';
   output += '<div class="redoc-json">';
-  output += '<code>';
+  //output += '<code>';
   output += valueToHTML(json, maxExpandLevel);
-  output += '</code>';
+  //output += '</code>';
   output += '</div>';
   return output;
 }
