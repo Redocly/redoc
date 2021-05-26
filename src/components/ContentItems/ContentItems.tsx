24c02e3c5218e24599377f2fdecb496e2dc8f49f
Alex Varchuk <olexandr.varchuk@gmail.com>
2021-05-26 11:26:14 +0300

    feat: add placeholder if no paths or webhooks

--- src/components/ContentItems/ContentItems.tsx
+++ src/components/ContentItems/ContentItems.tsx
@@ -15,7 +15,7 @@ export class ContentItems extends React.Component<{
   render() {
     const items = this.props.items;
     if (items.length === 0) {
-      return null;
+      return <MiddlePanel compact={false}>Cannot find path or webhooks items</MiddlePanel>;
     }
     return items.map(item => {
       return <ContentItem key={item.id} item={item} />;
