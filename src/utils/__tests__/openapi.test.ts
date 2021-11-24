26e676efb7a8d4eca0e86141c99677c44766818d
Oprysk <vyacheslav@redocly.com>
2021-11-24 16:19:20 +0200

    linting

--- src/utils/__tests__/openapi.test.ts
+++ src/utils/__tests__/openapi.test.ts
@@ -142,9 +142,9 @@ describe('Utils', () => {
       object: ['maxProperties', 'minProperties', 'required', 'additionalProperties', 'properties'],
     };
 
-    Object.keys(tests).forEach((name) => {
+    Object.keys(tests).forEach(name => {
       it(`Should detect ${name} if ${name} properties are present`, () => {
-        tests[name].forEach((propName) => {
+        tests[name].forEach(propName => {
           expect(
             detectType({
               [propName]: 0,
@@ -727,11 +727,11 @@ describe('Utils', () => {
       },
     ];
 
-    testCases.forEach((locationTestGroup) => {
+    testCases.forEach(locationTestGroup => {
       describe(locationTestGroup.description, () => {
-        locationTestGroup.cases.forEach((valueTypeTestGroup) => {
+        locationTestGroup.cases.forEach(valueTypeTestGroup => {
           describe(valueTypeTestGroup.description, () => {
-            valueTypeTestGroup.cases.forEach((testCase) => {
+            valueTypeTestGroup.cases.forEach(testCase => {
               it(`should serialize correctly when style is ${testCase.style} and explode is ${testCase.explode}`, () => {
                 const parameter: OpenAPIParameter = {
                   name: locationTestGroup.name,
