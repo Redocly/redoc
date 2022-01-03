2d37f4ee4f5c56a7c6931a5c2b9dbf6c5f927e46
Oprysk <vyacheslav@redocly.com>
2022-01-03 12:45:29 +0200

    chore: migrate from babel to esbuild loader

--- src/utils/test-utils.ts
+++ src/utils/test-utils.ts
@@ -1,6 +1,4 @@
-/* tslint:disable:no-implicit-dependencies */
-
-import { has, set } from 'lodash';
+import { objectHas, objectSet } from './object';
 
 function traverseComponent(root, fn) {
   if (!root) {
@@ -20,8 +18,8 @@ export function filterPropsDeep<T extends object>(component: T, paths: string[])
   traverseComponent(component, comp => {
     if (comp.props) {
       for (const path of paths) {
-        if (has(comp.props, path)) {
-          set(comp.props, path, '<<<filtered>>>');
+        if (objectHas(comp.props, path)) {
+          objectSet(comp.props, path, '<<<filtered>>>');
         }
       }
     }
