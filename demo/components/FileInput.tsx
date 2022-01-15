ea733f8fc8a61bb65ff251c57ca15849328d4d38
Depickere Sven <sven.depickere@hotmail.com>
2022-01-15 17:50:59 +0100

    feat(#1251): Add file selector to demo application
    This PR should fix the request to add a file selector to the demo application see issue #1251 .

---
+++ demo/components/FileInput.tsx
@@ -0,0 +1,56 @@
+import * as yaml from 'js-yaml';
+import * as React from 'react';
+import { RefObject, useRef } from 'react';
+import styled from '../../src/styled-components';
+
+const Button = styled.button`
+  background-color: #fff;
+  color: #333;
+  padding: 2px 10px;
+  touch-action: manipulation;
+  cursor: pointer;
+  user-select: none;
+  border: 1px solid #ccc;
+  font-size: 16px;
+  height: 28px;
+  box-sizing: border-box;
+  vertical-align: middle;
+  line-height: 1;
+  outline: none;
+  white-space: nowrap;
+  @media (max-width: 699px) {
+    display: none;
+  }
+`;
+
+function GetUseRef(): RefObject<HTMLInputElement> {
+  return useRef<HTMLInputElement>(null);
+}
+
+const FileInput = props => {
+  const hiddenFileInput: RefObject<HTMLInputElement> = GetUseRef();
+
+  function handleClick() {
+    if (hiddenFileInput && hiddenFileInput.current) {
+      hiddenFileInput.current.click();
+    }
+  }
+
+  function uploadFile(event) {
+    const file = event.target.files[0];
+    const reader = new FileReader();
+    reader.onload = () => {
+      props.onUpload(yaml.load(reader.result));
+    };
+    reader.readAsText(file);
+  }
+
+  return (
+    <span>
+      <Button onClick={handleClick}>Upload a file</Button>
+      <input type="file" style={{ display: 'none' }} onChange={uploadFile} ref={hiddenFileInput} />
+    </span>
+  );
+};
+
+export default FileInput;
