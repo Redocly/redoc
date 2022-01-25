8ded7699eed82346244308a5018526cd91cb4b24
romanhotsiy <gotsijroman@gmail.com>
2022-01-26 00:15:46 +0200

    fix: fix ssr usage after migrating to esbuild pipeline

--- webpack.config.ts
+++ webpack.config.ts
@@ -90,7 +90,7 @@ export default (env: { standalone?: boolean; browser?: boolean } = {}) => ({
       {
         test: /\.css$/,
         use: [
-          'style-loader',
+          'isomorphic-style-loader',
           'css-loader',
           {
             loader: 'esbuild-loader',
