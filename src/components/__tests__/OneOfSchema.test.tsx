f60b4758330dd756d670309827da60d3465b672a
Anastasiia <anastasiia@redocly.com>
2022-03-15 13:22:22 +0200

    Fix: deprecated badge on one of any of buttons  (#1930)

--- src/components/__tests__/OneOfSchema.test.tsx
+++ src/components/__tests__/OneOfSchema.test.tsx
@@ -6,18 +6,19 @@ import * as React from 'react';
 import { OneOfSchema, Schema } from '../';
 import { OpenAPIParser, SchemaModel } from '../../services';
 import { RedocNormalizedOptions } from '../../services/RedocNormalizedOptions';
+import { withTheme } from '../testProviders';
 
 const options = new RedocNormalizedOptions({});
 describe('Components', () => {
   describe('SchemaView', () => {
+    const parser = new OpenAPIParser(
+      { openapi: '3.0', info: { title: 'test', version: '0' }, paths: {} },
+      undefined,
+      options,
+    );
+
     describe('OneOf', () => {
       it('should pass down skipReadOnly/skipReadWrite to nested oneOf', () => {
-        const parser = new OpenAPIParser(
-          { openapi: '3.0', info: { title: 'test', version: '0' }, paths: {} },
-          undefined,
-          options,
-        );
-
         const schema = new SchemaModel(
           parser,
           { oneOf: [{ type: 'string' }, { type: 'integer' }] },
@@ -36,6 +37,20 @@ describe('Components', () => {
         expect(schemaViewElement.type).toEqual(OneOfSchema);
         expect(schemaViewElement.props.skipWriteOnly).toBeFalsy();
         expect(schemaViewElement.props.skipReadOnly).toBeTruthy();
+      });
+    });
+
+    describe('OneOf deprecated', () => {
+      const schema = new SchemaModel(
+        parser,
+        { oneOf: [{ type: 'string', deprecated: true }, { type: 'integer' }] },
+        '',
+        options,
+      );
+
+      it('should match snapshot', () => {
+        const component = shallow(withTheme(<Schema schema={schema} />));
+        expect(component.render()).toMatchSnapshot();
       });
     });
   });
