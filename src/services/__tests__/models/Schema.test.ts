99b1f3e468673f2e86e38fdb9eebd18c38570704
anastasiia-developer <anastasiia@redocly.com>
2022-05-04 11:59:43 +0300

    feat: Support OAS 3.1 unevaluatedProperties

--- src/services/__tests__/models/Schema.test.ts
+++ src/services/__tests__/models/Schema.test.ts
@@ -48,5 +48,33 @@ describe('Models', () => {
       expect(schema.fields).toHaveLength(1);
       expect(schema.pointer).toBe('#/components/schemas/Child');
     });
+
+    test('schemaDefinition should resolve unevaluatedProperties in properties', () => {
+      const spec = require('../fixtures/3.1/unevaluatedProperties.json');
+      parser = new OpenAPIParser(spec, undefined, opts);
+      const schema = new SchemaModel(parser, spec.components.schemas.Test, '', opts);
+      expect(schema.fields).toHaveLength(2);
+      expect(schema.fields![1].kind).toEqual('additionalProperties');
+      expect(schema.fields![1].schema.type).toEqual('any');
+    });
+
+    test('schemaDefinition should resolve unevaluatedProperties in anyOf', () => {
+      const spec = require('../fixtures/3.1/unevaluatedProperties.json');
+      parser = new OpenAPIParser(spec, undefined, opts);
+      const schema = new SchemaModel(parser, spec.components.schemas.Test2, '', opts);
+      expect(schema.oneOf![0].fields).toHaveLength(2);
+      expect(schema.oneOf![0].fields![1].kind).toEqual('additionalProperties');
+      expect(schema.oneOf![1].fields).toHaveLength(2);
+      expect(schema.oneOf![1].fields![1].kind).toEqual('additionalProperties');
+    });
+
+    test('schemaDefinition should resolve unevaluatedProperties type boolean', () => {
+      const spec = require('../fixtures/3.1/unevaluatedProperties.json');
+      parser = new OpenAPIParser(spec, undefined, opts);
+      const schema = new SchemaModel(parser, spec.components.schemas.Test3, '', opts);
+      expect(schema.fields).toHaveLength(2);
+      expect(schema.fields![1].kind).toEqual('additionalProperties');
+      expect(schema.fields![1].schema.type).toEqual('boolean');
+    });
   });
 });
