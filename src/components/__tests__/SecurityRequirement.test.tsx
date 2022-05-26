88a5864172b57813e24b4a964a5831550d967c8c
Anastasiia Derymarko <anastasiia@redocly.com>
2022-05-26 12:23:37 +0300

    test: move spec to fixtures

--- src/components/__tests__/SecurityRequirement.test.tsx
+++ src/components/__tests__/SecurityRequirement.test.tsx
@@ -11,122 +11,14 @@ import {
 import { StoreProvider } from '../StoreBuilder';
 import { SecurityRequirements } from '../SecurityRequirement/SecurityRequirement';
 import { withTheme } from '../testProviders';
-import { OpenAPISpec } from '../../types';
 import { SecurityDefs } from '../SecuritySchemes/SecuritySchemes';
-
-const spec: OpenAPISpec = {
-  openapi: '3.0',
-  info: {
-    title: 'test',
-    version: '0',
-  },
-  paths: {
-    '/pet': {
-      post: {
-        summary: 'Add a new pet to the store',
-        description: 'Add new pet to the store inventory.',
-        operationId: 'addPet',
-        responses: {
-          '405': {
-            description: 'Invalid input',
-          },
-        },
-        security: [
-          {
-            petstore_auth: ['write:pets', 'read:pets'],
-          },
-        ],
-      },
-      put: {
-        summary: 'Add a new pet to the store',
-        description: 'Add new pet to the store inventory.',
-        operationId: 'updatePet',
-        responses: {
-          '405': {
-            description: 'Invalid input',
-          },
-        },
-        security: [
-          {
-            GitLab_PersonalAccessToken: [],
-            GitLab_OAuth2Implicit: ['api'],
-            GitLab_OAuth2AuthorizationCode: ['api'],
-          },
-          {
-            petstore_auth: ['write:pets', 'read:pets'],
-          },
-        ],
-      },
-    },
-  },
-  components: {
-    securitySchemes: {
-      petstore_auth: {
-        description:
-          'Get access to data while protecting your account credentials.\nOAuth2 is also a safer and more secure way to give you access.\n',
-        type: 'oauth2',
-        bearerFormat: '',
-        flows: {
-          implicit: {
-            authorizationUrl: 'http://petstore.swagger.io/api/oauth/dialog',
-            scopes: {
-              'write:pets': 'modify pets in your account',
-              'read:pets': 'read your pets',
-            },
-          },
-        },
-      },
-      GitLab_PersonalAccessToken: {
-        description: 'GitLab Personal Access Token description',
-        type: 'apiKey',
-        name: 'PRIVATE-TOKEN',
-        in: 'header',
-        bearerFormat: '',
-        flows: {},
-      },
-      GitLab_OAuth2Implicit: {
-        description: 'OAuth2 Implicit description',
-        bearerFormat: '',
-        type: 'oauth2',
-        flows: {
-          implicit: {
-            authorizationUrl: 'https://gitlab.com/oauth/authorize',
-            scopes: {
-              api: 'Grants complete read/write access to the API, including all groups and projects, the container registry, and the package registry.',
-              read_user:
-                "Grants read-only access to the authenticated user's profile through the /user API endpoint, which includes username, public email, and full name. Also grants access to read-only API endpoints under /users.",
-              read_api:
-                'Grants read access to the API, including all groups and projects, the container registry, and the package registry.',
-              read_repository:
-                'Grants read-only access to repositories on private projects using Git-over-HTTP or the Repository Files API.',
-            },
-          },
-        },
-      },
-      GitLab_OAuth2AuthorizationCode: {
-        description: 'OAuth2 Authorization Code description',
-        type: 'oauth2',
-        bearerFormat: '',
-        flows: {
-          authorizationCode: {
-            tokenUrl: 'https://gitlab.com/oauth/token',
-            scopes: {
-              api: 'Grants complete read/write access to the API, including all groups and projects, the container registry, and the package registry.',
-              read_user:
-                "Grants read-only access to the authenticated user's profile through the /user API endpoint, which includes username, public email, and full name. Also grants access to read-only API endpoints under /users.",
-              read_api:
-                'Grants read access to the API, including all groups and projects, the container registry, and the package registry.',
-            },
-          },
-        },
-      },
-    },
-  },
-};
+import * as simpleSecurityFixture from './fixtures/simple-security-fixture.json';
 
 describe('SecurityRequirement', () => {
   it('should render authDefinition', async () => {
-    const store = await createStore(spec, undefined, { showSecuritySchemeType: true });
+    const store = await createStore(simpleSecurityFixture, undefined, {
+      showSecuritySchemeType: true,
+    });
 
     store.spec.contentItems.forEach((item: OperationModel) => {
       if (item.security) {
@@ -146,7 +38,11 @@ describe('SecurityRequirement', () => {
   });
 
   it('should render SecurityDefs', async () => {
-    const parser = new OpenAPIParser(spec, undefined, new RedocNormalizedOptions({}));
+    const parser = new OpenAPIParser(
+      simpleSecurityFixture,
+      undefined,
+      new RedocNormalizedOptions({}),
+    );
 
     const component = mount(
       withTheme(<SecurityDefs securitySchemes={new SecuritySchemesModel(parser)} />),
