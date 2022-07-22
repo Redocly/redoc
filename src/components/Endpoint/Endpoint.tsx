f5b05a72f7cf099a08c3d24ee196f83f7021a8db
Alex Varchuk <olexandr.varchuk@gmail.com>
2022-07-22 14:59:47 +0300

    Revert "fix: operation url in static page (#2093)"
    This reverts commit 98eec19647b63f3598ec30fdeb428f614cf93ad4.

--- src/components/Endpoint/Endpoint.tsx
+++ src/components/Endpoint/Endpoint.tsx
@@ -68,18 +68,18 @@ export class Endpoint extends React.Component<EndpointProps, EndpointState> {
                   ? expandDefaultServerVariables(server.url, server.variables)
                   : server.url;
                 const basePath = getBasePath(normalizedUrl);
-                const serverBaseUrl =
-                  hideHostname || options.hideHostname
-                    ? basePath === '/'
-                      ? ''
-                      : basePath
-                    : normalizedUrl;
                 return (
                   <ServerItem key={normalizedUrl}>
                     <Markdown source={server.description || ''} compact={true} />
                     <SelectOnClick>
                       <ServerUrl>
-                        <span>{serverBaseUrl}</span>
+                        <span>
+                          {hideHostname || options.hideHostname
+                            ? basePath === '/'
+                              ? ''
+                              : basePath
+                            : normalizedUrl}
+                        </span>
                         {operation.path}
                       </ServerUrl>
                     </SelectOnClick>
