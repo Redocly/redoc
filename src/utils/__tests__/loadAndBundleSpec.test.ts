2238244f0065f4e240aac4ebc3146a7688cc332f
Brian <bgome004@gmail.com>
2022-11-14 14:37:40 -0500

    Fixed snapshot test

--- src/utils/__tests__/loadAndBundleSpec.test.ts
+++ src/utils/__tests__/loadAndBundleSpec.test.ts
@@ -4,6 +4,12 @@ import { resolve } from 'path';
 import { loadAndBundleSpec } from '../loadAndBundleSpec';
 
 describe('#loadAndBundleSpec', () => {
+  it('should load And Bundle Spec demo/openapi.yaml', async () => {
+    const spec = yaml.load(readFileSync(resolve(__dirname, '../../../demo/openapi.yaml'), 'utf-8'));
+    const bundledSpec = await loadAndBundleSpec(spec);
+    expect(bundledSpec).toMatchSnapshot();
+  });
+
   it('should load And Bundle Spec demo/openapi-3-1.yaml', async () => {
     const spec = yaml.load(
       readFileSync(resolve(__dirname, '../../../demo/openapi-3-1.yaml'), 'utf-8'),
