44346dc46d40525c52accc1022a264e69f2a2474
Brian <bgome004@gmail.com>
2022-11-14 11:34:33 -0500

    DX-2758 Removed redocly attribution at the bottom of the sidebar

--- src/components/SideMenu/SideMenu.tsx
+++ src/components/SideMenu/SideMenu.tsx
@@ -26,12 +26,6 @@ export class SideMenu extends React.Component<{ menu: MenuStore; className?: str
         }}
       >
         <MenuItems items={store.items} onActivate={this.activate} root={true} />
-        <RedocAttribution>
-          <a target="_blank" rel="noopener noreferrer" href="https://redocly.com/redoc/">
-            <RedoclyLogo />
-            API docs by Redocly
-          </a>
-        </RedocAttribution>
       </PerfectScrollbarWrap>
     );
   }
