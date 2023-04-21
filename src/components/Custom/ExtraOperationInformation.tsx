9cc2e672e3936a55e31d484180c6c51c9d79a947
Depickere Sven <sven.depickere@hotmail.com>
2023-04-21 14:19:19 +0200

    feat(): Add styling

--- src/components/Custom/ExtraOperationInformation.tsx
+++ src/components/Custom/ExtraOperationInformation.tsx
@@ -5,17 +5,19 @@ import { observer } from 'mobx-react';
 import { shortenHTTPVerb } from '../../utils';
 
 export interface ExtraOperationInformationProps {
+  description: string;
   httpVerb: string;
   label: string;
 }
 
 export const ExtraOperationInformation = observer((props: ExtraOperationInformationProps) => {
-  const item = props;
-
   return (
-    <div>
-      <OperationBadge type={item.httpVerb}>{shortenHTTPVerb(item.httpVerb)}</OperationBadge>
-      <MenuItemTitle width="calc(100% - 38px)">{item.label}</MenuItemTitle>
+    <div style={{ padding: '12.5px 0', margin: '0.5rem 0' }}>
+      <div>{props.description}</div>
+      <div style={{ display: 'flex' }}>
+        <OperationBadge type={props.httpVerb}>{shortenHTTPVerb(props.httpVerb)}</OperationBadge>
+        <MenuItemTitle width="calc(100% - 38px)">{props.label}</MenuItemTitle>
+      </div>
     </div>
   );
 });
