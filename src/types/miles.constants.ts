125fb6aa079403c04d9c9ddd42ead9ff297539de
Depickere Sven <sven.depickere@hotmail.com>
2023-04-21 10:48:25 +0200

    feat(): Add new extra property

--- src/types/miles.constants.ts
+++ src/types/miles.constants.ts
@@ -1,4 +1,5 @@
 export const MilesConstants = {
   MILES_EXTRA_DESCRIPTION_PROPERTY_NAME: 'x-miles-extra-description',
+  MILES_VALIDATION_MODEL_PROPERTY_NAME: 'x-miles-validation-model',
   HIDE_LOGO_QUERY_PARAM: 'hideLogo',
 };
