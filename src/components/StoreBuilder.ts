0e2d595ef7be449fb8c5c99bf348778dbb6f054c
Alex Varchuk <olexandr.varchuk@gmail.com>
2025-01-16 12:01:29 +0100

    chore: fix vulnerabilities (#2648)

--- src/components/StoreBuilder.ts
+++ src/components/StoreBuilder.ts
@@ -56,6 +56,7 @@ export function StoreBuilder(props: StoreBuilderProps) {
       }
     }
     load();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [spec, specUrl]);
 
   const store = React.useMemo(() => {
